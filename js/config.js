6249b024c73920bffa0bc38f0c157698d444a29d
Vasanthag <68294117+vasantha9099@users.noreply.github.com>
2021-05-24 17:22:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_IyuKbbtxM',
         userPoolClientId: '4hrhmk75htced5s1s4rm2lm3jh',
-        region: 'eu-west-1'
+        region: 'eu-east-2'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
