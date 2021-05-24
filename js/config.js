1fa516fe943a64533f3b5b8245d0f10e5dc1bf56
Vasanthag <68294117+vasantha9099@users.noreply.github.com>
2021-05-24 17:28:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_IyuKbbtxM',
-        userPoolClientId: '4hrhmk75htced5s1s4rm2lm3jh',
+        userPoolId: 'us-east-2_eTlKfTbOB',
+        userPoolClientId: '14pceiefhtt790bu4fuulaijt7',
         region: 'eu-east-2'
     },
     api: {
